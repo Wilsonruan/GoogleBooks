8228e48d25ed349676a52ca0a9a497d09d2a0d6e
Wilson Ruan <wilsonruan23@gmail.com>
2020-12-14 20:32:10 -0500

    Inits yarn build

---
+++ client/build/precache-manifest.890af6e6fa1b8bdbc4e942884cc0dfe7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a1ae21fce5fbc1cfa06",
+    "url": "/static/css/main.bb70793d.chunk.css"
+  },
+  {
+    "revision": "1a1ae21fce5fbc1cfa06",
+    "url": "/static/js/main.2067e67d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d1d516c1d6d521985ed9",
+    "url": "/static/js/2.b454d17e.chunk.js"
+  },
+  {
+    "revision": "87787b5374961dfe922e9091276a3b20",
+    "url": "/index.html"
+  }
+];
